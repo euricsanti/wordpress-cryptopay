5f1f2fc549accf4f93c2c7c11a42f9a1b898f73e
Euric Santi <euricsanti@gmai.com>
2019-04-24 14:12:01 -0400

    new

---
+++ somenano-install.php
@@ -0,0 +1,56 @@
+<?php
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+
+// REF: https://codex.wordpress.org/Creating_Tables_with_Plugins
+
+global $somenano_db_version;
+$somenano_db_version = '1.0.0';
+
+global $somenano_version;
+$somenano_version = '0.1.0';
+
+function somenano_install()
+{
+    global $wpdb;
+    global $somenano_db_version;
+
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $table_name = somenano_default('db_payments');
+    $sql = "CREATE TABLE $table_name (
+        token varchar(300) NOT NULL,
+        dtg TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        received_rai int UNSIGNED NOT NULL,
+        currency varchar(4) NOT NULL,
+        currency_amount varchar(20) NOT NULL,
+        post_id mediumint(9) UNSIGNED NOT NULL,
+        user_id smallint UNSIGNED NOT NULL,
+        block varchar(65) NOT NULL,
+        PRIMARY KEY (token)
+    ) $charset_collate;";
+    dbDelta( $sql );
+
+    $table_name = somenano_default('db_paywalls');
+    $sql = "CREATE TABLE $table_name (
+        dtg TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        currency varchar(4) NOT NULL,
+        currency_amount varchar(20) NOT NULL,
+        post_id mediumint(9) UNSIGNED NOT NULL,
+        destination varchar(70) NOT NULL,
+        PRIMARY KEY (post_id)
+    ) $charset_collate;";
+    dbDelta( $sql );
+
+    add_option( '$somenano_db_version', $somenano_db_version );
+    add_option( '$somenano_version', $somenano_version );
+}
+
+function somenano_install_data()
+{
+
+}
+
+?>
---
+++ somenano-log.php
@@ -0,0 +1,128 @@
+<?php
+
+require_once( 'somenano-brainblocks.php' );
+require_once( 'somenano-defaults.php' );
+
+function somenano_has_log_paywall($currency, $currency_amount, $post_id, $destination)
+{
+    global $wpdb;
+
+    $sql = 'select count(*) from ' . somenano_default('db_paywalls') . ' where currency = "' . $currency . '" and currency_amount = ' . $currency_amount . ' and post_id = ' . $post_id . ' and destination = "' . $destination . '"';
+    $num_rows = $wpdb->get_var( $sql );
+    if ( $num_rows > 0 ) return true;
+    return false;
+}
+
+function somenano_log_paywall($currency, $currency_amount, $post_id, $destination)
+{
+    global $wpdb;
+
+    // Validate inputs
+    if ( !in_array( $currency, somenano_bb_currencies() ) ) {
+        error_log('somenano-log: invalid currency: '. $currency);
+        return false;
+    }
+
+    if ( !is_numeric( $currency_amount ) ) {
+        error_log('somenano-log: invalid currency_amount: '. $currency_amount);
+        return false;
+    }
+
+    if ( !is_numeric( $post_id ) ) {
+        error_log('somenano-log: invalid post_id: '. $post_id);
+        return false;
+    }
+
+    if ( !preg_match( '/^(xrb|nano)_[a-z0-9]{60}$/i', $destination ) ) {
+        error_log('somenano-log: invalid destination: '. $destination);
+        return false;
+    }
+
+    $wpdb->replace(
+        somenano_default('db_paywalls'),
+        array(
+            'currency' => $currency,
+            'currency_amount' => $currency_amount,
+            'post_id' => $post_id,
+            'destination' => $destination
+        )
+    );
+    return true;
+}
+
+function somenano_log($token, $post_id)
+{
+
+    global $wpdb;
+
+    // Validate Inputs
+    if ( !ctype_alnum($token) ) {
+        error_log('somenano-log: token has invalid characters');
+        return false;
+    }
+
+    if ( !is_numeric($post_id) ) {
+        error_log('somenano-log: post_id is invalid value');
+        return false;
+    }
+
+    $token_data = somenano_bb_token( $token );
+    if ( $token_data == false ) {
+        error_log('somenano-log: BrainBlocks did not return successfully');
+        return false;
+    }
+
+    if ( array_key_exists('status', $token_data) && $token_data['status'] == 'error' ) {
+        error_log('somenano-log: BrainBlocks returned error status');
+        return false;
+    }
+
+    if ( !isset($token_data['received_rai']) ) {
+        error_log('somenano-log: BrainBlocks token did not return received_rai');
+        return false;
+    }
+
+    if ( !isset($token_data['send_block']) ) {
+        error_log('somenano-log: BrainBlocks token did not return send_block');
+        return false;
+    }
+
+    if ( !isset($token_data['currency']) ) {
+        error_log('somenano-log: BrainBlocks token did not return currency');
+        return false;
+    }
+
+    if ( !isset($token_data['amount']) ) {
+        error_log('somenano-log: BrainBlocks token did not return amount');
+        return false;
+    }
+
+    // Check if paywall is in the database
+    if ( !somenano_has_log_paywall( $token_data['currency'], $token_data['amount'], $post_id, $token_data['destination'] ) ) {
+        error_log('somenano-log: No paywall found in database');
+        return false;
+    }
+
+    // Update database
+    $wpdb->insert(
+        somenano_default('db_payments'),
+        array(
+            'token' => $token,
+            'received_rai' => $token_data['received_rai'],
+            'post_id' => $post_id,
+            'user_id' => get_current_user_id(),
+            'block' => $token_data['send_block'],
+            'currency' => $token_data['currency'],
+            'currency_amount' => $token_data['amount']
+        )
+    );
+
+    // Save cookie
+
+    setcookie( somenano_default('cookie_prefix').$post_id, $token, somenano_default('cookie_expiration'), '/' );
+
+    return true;
+
+}
+
+?>
